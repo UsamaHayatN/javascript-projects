eb08a0c54a6872acd778471688023bc3d70e35b0
UsamaHayatN <usamahayatnissoana@gmail.com>
2024-10-02 19:15:27 -0700

    Updating: MY 3 javascript Projects

---
+++ Forkify-app/src/js/controller.js
@@ -0,0 +1,37 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id)
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    alert(err)
+    // console.log(err)
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes)
+}
+init();
